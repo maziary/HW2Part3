0d0a2cbc517bef515cbdb850c8144ca1fbb5574a
maziary <maziar.yaesoubi@gmail.com>
2012-10-03 21:01:53 -0600

    HW2Part3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HW2Blog::Application.config.secret_token = '5039b148f3ea14b66fa579ec0b322a72a718d126ec7de29b1369a39590b2dade7dcc8047a6aed5556b0388972778385b0b6381bcdb16f6ca99c694d33af27e8d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HW2Blog::Application.config.session_store :cookie_store, key: '_HW2Blog_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HW2Blog::Application.config.session_store :active_record_store
